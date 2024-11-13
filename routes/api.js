bfe003fae1bfb736b748ac69dff51a8ebc600df9
annalisa4sec <122273976+annalisa4sec@users.noreply.github.com>
2024-11-13 22:19:17 +0100

    pass all POST tests

--- routes/api.js
+++ routes/api.js
@@ -16,17 +16,16 @@ module.exports = function (app) {
         const data = await Book.find()
         const books = data.map(b => ({ _id: b._id, title: b.title, commentcount: b.commentcount ?? 0 }))
         res.json(books)
-        //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]}
       } catch (err) {
         console.error("Error retrieving books:\n", err);
       }
     })
 
     .post(async (req, res) => {
-      let title = req.body.title;
+      const title = req.body.title;
       try {
         if (!title) {
-          return res.json({ error: "missing required field title" })
+          return res.send('missing required field title')
         }
         const book = new Book({
           title,
